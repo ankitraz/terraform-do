8870cf3c331e73bbe4edc3edc9d839a6860f9719
Ankit <you@example.com>
2023-03-18 16:48:09 +0530

    added files

--- main.tf
+++ main.tf
@@ -12,59 +12,16 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-# Create a web server
-resource "digitalocean_droplet" "web" {
-  name     = "my-droplet"
-  size     = "s-1vcpu-1gb"
-  image    = "ubuntu-18-04-x64"
-  region   = "BLR1"
-  ssh_keys = [digitalocean_ssh_key.droplet-ssh-key.fingerprint]
-}
+resource "digitalocean_kubernetes_cluster" "my-cluster" {
+  name    = "my-cluster"
+  region  = "blr1"
+  version = "1.25.4-do.0"
 
-
-# Create a new SSH key
-resource "digitalocean_ssh_key" "droplet-ssh-key" {
-  name       = "Terraform Example"
-  public_key = var.ssh_key                     //file(var.ssh_key)
-}
-
-//this will give ipv4 address of our newly created droplet
-output "droplet_ip" {
-  value = digitalocean_droplet.web.ipv4_address
-}
-
-resource "digitalocean_firewall" "web" {
-  name        = "only-22-80-firewall"
-  droplet_ids = [digitalocean_droplet.web.id]
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "22"
-    source_addresses = ["0.0.0.0/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "80"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-
-  outbound_rule {
-    protocol              = "tcp"
-    port_range            = "1-65535"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol              = "icmp"
-    port_range            = "1-65535"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol              = "udp"
-    port_range            = "1-65535"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
+  node_pool {
+    name       = "autoscale-worker-pool"
+    size       = "s-2vcpu-2gb"
+    auto_scale = true
+    min_nodes  = 1
+    max_nodes  = 5
   }
 }
